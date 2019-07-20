76a89ca23274fc46f7baeeb287c9f7f629031255
Tom <tom@aruxsoftware.com>
2019-07-20 15:22:03 -0500

    add the field to here

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@
 module Types
   # This is the base mutation type. All mutations inherit from it.
   class MutationType < Types::BaseObject
+    field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
   end
 end
