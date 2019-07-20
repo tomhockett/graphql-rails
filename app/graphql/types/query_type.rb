3428e7824a9632f95b80aac2d85e8834d27e19a5
Tom <tom@aruxsoftware.com>
2019-07-20 14:29:40 -0500

    set the all_links resolver

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,15 @@
-module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+# frozen_string_literal: true
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+module Types
+  # Root for all GraphQL queries.
+  class QueryType < BaseObject
+    # queries are just represented as fields.
+    # `all_links` is automatically camelcased to `allLinks`.
+    field :all_links, [LinkType], null: false
+
+    # this method is invoked, when `all_link` fields is being resolved.
+    def all_links
+      Link.all
     end
   end
 end
